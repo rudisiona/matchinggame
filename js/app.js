4e0907a8a9540affb881226dbd187007ca088b1a
rudisiona <rsmageo@gmail.com>
2025-05-06 19:25:43 -0700

    board format and card flip

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 console.log('the world is yours')
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+const cards = document.querySelectorAll('.card')
+
+/*-------------------------------- Functions --------------------------------*/
+function flipCard() {
+   this.classList.toggle('flip')
+}
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+cards.forEach(card => card.addEventListener('click', flipCard))
+
