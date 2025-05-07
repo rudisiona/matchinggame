937af78284018ac7418e190c9dc2cb57e0c181a5
rudisiona <rsmageo@gmail.com>
2025-05-06 20:34:29 -0700

    start button

--- js/app.js
+++ js/app.js
@@ -11,16 +11,34 @@ console.log('the world is yours')
 /*------------------------ Cached Element References ------------------------*/
 
 const cards = document.querySelectorAll('.card')
+const levels = document.querySelectorAll('.level')
+const timer = document.querySelector('#timer')
+const start = document.querySelector('#startBtn')
 
 /*-------------------------------- Functions --------------------------------*/
 function flipCard() {
    this.classList.toggle('flip');
    console.log(this)
 }
+function setTimer() {
+  if(this.innerText === 'Easy') {
+timer.textContent = '2:00';
+  } else if(this.innerText === 'Medium'){
+   timer.textContent = '1:00'
+  } else {
+   timer.textContent = '0:30'
+  }
+}
+function startTimer() {
+   
+}
+
+
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 cards.forEach(card => card.addEventListener('click', flipCard))
-
+levels.forEach(level => level.addEventListener('click', setTimer))
+start.addEventListener('click', startTimer)
