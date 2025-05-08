d8dc2b342f089639c6257c9643a65b128fa16cb6
rudisiona <rsmageo@gmail.com>
2025-05-07 19:07:09 -0700

    working coundtown timer

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@ console.log('the world is yours')
 
 /*-------------------------------- Constants --------------------------------*/
 
-
+var countDown;
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let countdown = 0
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -22,16 +22,40 @@ function flipCard() {
 }
 function setTimer() {
   if(this.innerText === 'Easy') {
-timer.textContent = '2:00';
+      timer.textContent = '2:00';
+      countdown = 120;
+
   } else if(this.innerText === 'Medium'){
-   timer.textContent = '1:00'
+        timer.textContent = '1:00';
+        countdown = 60;
+
   } else {
-   timer.textContent = '0:30'
+        timer.textContent = '0:30';
+        countdown = 30;
+  
   }
 }
 function startTimer() {
-   
+
+  countDown = setInterval(function() {
+    if (countdown == 0) {
+      clearInterval(countDown);
+      timer.textContent = '0:00'
+    } else {
+  const minutes = Math.floor(countdown / 60)
+      let seconds = countdown % 60
+      seconds = seconds < 10 ? '0' + seconds : seconds;
+          timer.textContent = `${minutes}:${seconds}`
 }
+    
+   countdown -= 1;
+      }, 1000);
+
+
+}
+
+
+
 
 
 
@@ -42,3 +66,5 @@ function startTimer() {
 cards.forEach(card => card.addEventListener('click', flipCard))
 levels.forEach(level => level.addEventListener('click', setTimer))
 start.addEventListener('click', startTimer)
+// const starts = setInterval(startTimer, 1000)
+
