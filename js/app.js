be25c227bd25275515b3206c3417f68d0ee7f096
rudisiona <rsmageo@gmail.com>
2025-05-11 20:46:36 -0700

    working win logic and style

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 console.log('the world is yours')
 
+console.log('the world is yours')
+
 /*-------------------------------- Constants --------------------------------*/
 
 var countDown;
@@ -10,7 +12,8 @@ let countdown = 0;
 let matched = [];
 let hasFlippedCard = false;
 let firstCard, secondCard;
- 
+let timerFinished = false;
+let checkWinInterval;
 /*------------------------ Cached Element References ------------------------*/
 const board = document.querySelector('#gameBoard')
 const cards = document.querySelectorAll('.card')
@@ -25,7 +28,10 @@ const cardArray = Array.from(cards);
 /*-------------------------------- Functions --------------------------------*/
 
 function flipCard() {
+ 
+
    this.classList.toggle('flip');
+  
    if(!hasFlippedCard){
     hasFlippedCard = true;
     firstCard = this;
@@ -36,7 +42,7 @@ function flipCard() {
    if(firstCard.dataset.key === secondCard.dataset.key){
     firstCard.removeEventListener('click', flipCard)
     secondCard.removeEventListener('click', flipCard)
-    checkWin(cardArray)
+    
    } else {
     setTimeout(() => {
       firstCard.classList.remove('flip')
@@ -46,38 +52,48 @@ function flipCard() {
    } 
 }
 
-function checkWin(cardArray) {
-const matchedBoard = cardArray.every(card => card.className === cardArray[0].className)
+
+function checkWin() {
+const matchedBoard = cardArray.every(card => card.classList.contains('flip'))
 console.log(cardArray)
 if(matchedBoard){
     console.log('match')
     clearInterval(countDown)
     message.textContent = `congrats you have great memory`
-} else {
+    clearInterval(checkWinInterval)
+} else if (!matchedBoard && timer.textContent === '0:00') {
+    message.textContent = '.. you should probably work on your memory.. try again?'
+    restart.innerHTML = 'Try Again'
+    clearInterval(checkWinInterval)
+    cards.forEach(card => card.removeEventListener('click', flipCard))
+} else{
     console.log('no match')
 }
 
 }
+
 function setTimer() {
-  if(this.innerText === 'Easy') {
+  if(this.innerText === '2:00') {
       timer.textContent = '2:00';
-      countdown = 1200;
-  } else if(this.innerText === 'Medium'){
+      countdown = 120;
+  } else if(this.innerText === '1:00'){
         timer.textContent = '1:00';
         countdown = 60;
   } else {
         timer.textContent = '0:30';
-        countdown = 30;
+        countdown = 5;
    }       
   start.addEventListener('click', startGame)
 }
 
 function startTimer() {
   countDown = setInterval(function() {
-    if (countdown == 0) {
+    if (countdown === 0) {
       clearInterval(countDown);
-      timer.textContent = '0:00'
+      timer.textContent = '0:00';
+      timerFinished = true;
     } else {
+        message.textContent = 'Go'
         const minutes = Math.floor(countdown / 60)
         let seconds = countdown % 60
         seconds = seconds < 10 ? '0' + seconds : seconds;
@@ -109,6 +125,8 @@ startTimer()
 start.removeEventListener('click', startGame);
 cards.forEach(card => card.addEventListener('click', flipCard))
 restart.addEventListener('click', restartGame)
+checkWinInterval = setInterval(checkWin, 1000);
+
 
 
 
@@ -117,7 +135,10 @@ restart.addEventListener('click', restartGame)
 function restartGame() {
   timer.textContent = '0:00'
   countdown = 0
-if (flipped) {
+  restart.textContent = 'restart'
+  clearInterval(checkWinInterval)
+  message.textContent = 'forgot how to play? choose a difficulty to set the timer. press the start button. find all the matches before the timer runs out.'
+if(flipped) {
   cards.forEach(card => {
     card.classList.remove('flip');
   });
@@ -127,10 +148,12 @@ if (flipped) {
 }
 
 
-
-
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 levels.forEach(level => level.addEventListener('click', setTimer))
 start.addEventListener('click', startGame)
+
+
+
+
